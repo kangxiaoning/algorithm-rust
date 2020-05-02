545f8b5cbdd428177d9179e91c11a369befe2646
kangxiaoning <kxnmei@163.com>
2020-05-02 22:13:37 +0800

    add insertion partial sorting

--- src/sorting/insertion.rs
+++ src/sorting/insertion.rs
@@ -38,6 +38,23 @@ pub fn sort_v2<T: Ord + Copy>(arr: &mut [T]) {
     }
 }
 
+pub fn sort_v3<T: Ord + Copy>(arr: &mut [T], start: usize, end: usize) {
+    for i in (start + 1)..(end + 1) {
+        let e = arr[i];
+        // 在 arr[start+1..i] 之间寻找合适的插入位置
+        let mut left: usize = i;
+        for j in ((start + 1)..(i + 1)).rev() {
+            // 如果当前元素比前面的元素小刚交换位置
+            if e < arr[j - 1] {
+                arr[j] = arr[j - 1];
+                left -= 1;
+            } else {
+                break;
+            }
+        }
+        arr[left] = e;
+    }
+}
 // 简单测试sort的时间复杂度
 // 对不同数量的元素进行排序，观察元素数量和排序时间的关系
 pub fn run() {
@@ -110,6 +127,14 @@ mod tests {
         let mut res = vec![4, 1, 8, 5, 7];
         sort_v2(&mut res);
         assert_eq!(res, vec![1, 4, 5, 7, 8]);
+        // total sorting
+        let mut res = vec![4, 1, 8, 5, 7];
+        sort_v3(&mut res, 0, 4);
+        assert_eq!(res, vec![1, 4, 5, 7, 8]);
+        // partial sorting
+        let mut res = vec![4, 1, 8, 5, 7];
+        sort_v3(&mut res, 1, 3);
+        assert_eq!(res, vec![4, 1, 5, 8, 7]);
     }
 
     #[test]
@@ -120,6 +145,14 @@ mod tests {
         let mut res = vec!['A', 'a', 'h', 'b', 'W'];
         sort_v2(&mut res);
         assert_eq!(res, vec!['A', 'W', 'a', 'b', 'h']);
+        // total sorting
+        let mut res = vec!['A', 'a', 'h', 'b', 'W'];
+        sort_v3(&mut res, 0, 4);
+        assert_eq!(res, vec!['A', 'W', 'a', 'b', 'h']);
+        // partial sorting
+        let mut res = vec!['A', 'a', 'h', 'b', 'W'];
+        sort_v3(&mut res, 1, 3);
+        assert_eq!(res, vec!['A', 'a', 'b', 'h', 'W']);
     }
 
     #[test]
@@ -130,6 +163,9 @@ mod tests {
         let mut res = Vec::<u8>::new();
         sort_v2(&mut res);
         assert_eq!(res, vec![]);
+        let mut res = Vec::<u8>::new();
+        sort_v3(&mut res, 0, 0);
+        assert_eq!(res, vec![]);
     }
 
     #[test]
@@ -139,6 +175,9 @@ mod tests {
         assert_eq!(res, vec!['a', 'b', 'c', 'd']);
         let mut res = vec!['a', 'b', 'c', 'd'];
         sort_v2(&mut res);
+        assert_eq!(res, vec!['a', 'b', 'c', 'd']);
+        let mut res = vec!['a', 'b', 'c', 'd'];
+        sort_v3(&mut res, 0, 3);
         assert_eq!(res, vec!['a', 'b', 'c', 'd']);
     }
 }
