6e3a1323f2001824e48e14acdd784bdcd847ab77
kangxiaoning <kxnmei@163.com>
2020-05-17 20:42:15 +0800

    add union find v6 - path compression by cursion

--- src/uf/unionfind.rs
+++ src/uf/unionfind.rs
@@ -316,6 +316,78 @@ pub mod uf5 {
     }
 }
 
+// 第六版Union-Find
+pub mod uf6 {
+    pub struct UnionFind {
+        // parent[i]表示第i个元素所指向的父节点
+        parent: Vec<usize>,
+        // rank[i]表示以i为根的集合所表示的树的层数
+        // rank的值在路径压缩的过程中, 有可能不在是树的层数值
+        // 这也是rank不叫height或者depth的原因, 他只是作为比较的一个标准
+        rank: Vec<usize>,
+        count: usize,
+    }
+
+    impl UnionFind {
+        pub fn new(n: usize) -> Self {
+            let rank = vec![1; n];
+            let mut parent = vec![0; n];
+            for i in 0..n {
+                parent[i] = i;
+            }
+
+            Self {
+                parent,
+                rank,
+                count: n,
+            }
+        }
+
+        // 查找过程, 查找元素p所对应的集合编号
+        // O(h)复杂度, h为树的高度
+        pub fn find(&mut self, p: usize) -> usize {
+            assert!(p < self.count);
+            // 不断去查询自己的父亲节点, 直到到达根节点
+            // 根节点的特点: parent[p] == p
+
+            // path compression 2
+            // recursion
+            if p != self.parent[p] {
+                self.parent[p] = self.find(self.parent[p]);
+            }
+            self.parent[p]
+        }
+
+        // 查看元素p和元素q是否所属一个集合
+        // O(h)复杂度, h为树的高度
+        pub fn is_connected(&mut self, p: usize, q: usize) -> bool {
+            self.find(p) == self.find(q)
+        }
+
+        // 合并元素p和元素q所属的集合
+        // O(h)复杂度, h为树的高度
+        pub fn union_elements(&mut self, p: usize, q: usize) {
+            let p_root = self.find(p);
+            let q_root = self.find(q);
+
+            if p_root == q_root {
+                return;
+            }
+            // 根据两个元素所在树的层次不同判断合并方向
+            // 将层次较小的集合合并到层次较大的集合上
+            if self.rank[p_root] < self.rank[q_root] {
+                self.parent[p_root] = q_root;
+            } else if self.rank[p_root] > self.rank[q_root] {
+                self.parent[q_root] = p_root;
+            } else {
+                self.parent[p_root] = q_root;
+                // 此时维护 rank 值
+                self.rank[q_root] += 1;
+            }
+        }
+    }
+}
+
 pub mod union_find_test_helper {
     use rand::{self, Rng};
     use std::time::Instant;
@@ -439,6 +511,30 @@ pub mod union_find_test_helper {
         // 打印输出对这2n个操作的耗时
         println!("UF5, {} ops, {} µs", 2 * n, now.elapsed().as_micros());
     }
+
+    pub fn test_uf6(n: usize) {
+        let mut uf = super::uf6::UnionFind::new(n);
+        let mut rng = rand::thread_rng();
+
+        // 测试运行时间
+        let now = Instant::now();
+
+        // 进行n次操作，每次随机选择两个元素进行合并操作
+        for _ in 0..n {
+            let a = rng.gen_range(0, n);
+            let b = rng.gen_range(0, n);
+            uf.union_elements(a, b);
+        }
+
+        // 进行n次操作，每次随机选择两个元素，查询是否属于同一个集合
+        for _ in 0..n {
+            let a = rng.gen_range(0, n);
+            let b = rng.gen_range(0, n);
+            uf.is_connected(a, b);
+        }
+        // 打印输出对这2n个操作的耗时
+        println!("UF6, {} ops, {} µs", 2 * n, now.elapsed().as_micros());
+    }
 }
 
 pub fn run() {
@@ -449,4 +545,5 @@ pub fn run() {
     union_find_test_helper::test_uf3(n);
     union_find_test_helper::test_uf4(n);
     union_find_test_helper::test_uf5(n);
+    union_find_test_helper::test_uf6(n);
 }
