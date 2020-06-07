82ab5375fcbcebee9fc355acf8427a059a479944
kangxiaoning <kxnmei@163.com>
2020-06-07 16:16:40 +0800

    Implement Ord for Edge<T> manually

--- src/graph/edge.rs
+++ src/graph/edge.rs
@@ -1,12 +1,41 @@
-use std::cmp::{Eq, PartialEq, PartialOrd};
+use std::cmp::{Eq, Ordering, PartialEq, PartialOrd};
 use std::fmt;
 
 // 有权边
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone)]
 pub struct Edge<T> {
     a: usize,
     b: usize,
     weight: T,
+}
+
+impl<T> PartialEq for Edge<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.weight == other.weight
+    }
+}
+
+impl<T> Eq for Edge<T> where T: PartialEq {}
+
+impl<T> PartialOrd for Edge<T>
+where
+    T: PartialEq + PartialOrd,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.weight.partial_cmp(&other.weight)
+    }
+}
+
+impl<T> Ord for Edge<T>
+where
+    T: PartialEq + PartialOrd + Eq,
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.weight.partial_cmp(&other.weight).unwrap()
+    }
 }
 
 impl<T> fmt::Display for Edge<T>
@@ -14,13 +43,13 @@ where
     T: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}-{:.2}-{}", self.a, self.weight, self.b)
+        write!(f, "{}-{}: {:.2}", self.a, self.b, self.weight)
     }
 }
 
 impl<T> Edge<T>
 where
-    T: Copy,
+    T: Clone,
 {
     pub fn new(a: usize, b: usize, weight: T) -> Self {
         Self { a, b, weight }
@@ -35,7 +64,7 @@ where
     }
 
     pub fn weight(&self) -> T {
-        self.weight
+        self.weight.clone()
     }
 
     pub fn other(&self, x: usize) -> usize {
