1765fe39a64335b5a5c7b02ee8103d881219f27e
kangxiaoning <kxnmei@163.com>
2020-06-20 21:29:04 +0800

    add quick sort v2

--- src/sorting/quick.rs
+++ src/sorting/quick.rs
@@ -1,7 +1,8 @@
 use crate::util;
+use rand::{self, Rng};
 
 // 对arr[low:high]进行partition操作，将第1个元素放在排序后的位置
-fn partition<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) -> usize {
+fn partition_v1<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) -> usize {
     // pick first element as pivot
     let e = arr[low].clone();
 
@@ -21,22 +22,70 @@ fn partition<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) -> usize {
     j
 }
 
-fn quick_sort<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) {
+fn quick_sort_v1<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) {
     if low >= high {
         return;
     }
 
-    let p = partition(arr, low, high);
+    let p = partition_v1(arr, low, high);
     if p > 0 {
-        quick_sort(arr, low, p - 1);
+        quick_sort_v1(arr, low, p - 1);
     }
-    quick_sort(arr, p + 1, high);
+    quick_sort_v1(arr, p + 1, high);
 }
 
 pub fn sort_v1<T: Ord + Clone>(arr: &mut [T]) {
     if arr.len() > 0 {
         let high = arr.len() - 1;
-        quick_sort(arr, 0, high)
+        quick_sort_v1(arr, 0, high)
+    }
+}
+
+// 优化一：小规模数组使用insertion sort进行优化
+fn quick_sort_v2<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) {
+    if low >= high {
+        return;
+    }
+
+    if high - low <= 15 {
+        super::insertion::sort_v3(arr, low, high);
+        return;
+    }
+
+    let p = partition_v2(arr, low, high);
+    if p > 0 {
+        quick_sort_v2(arr, low, p - 1);
+    }
+    quick_sort_v2(arr, p + 1, high);
+}
+
+// 优化二：选择随机数作为pivot
+fn partition_v2<T: Ord + Clone>(arr: &mut [T], low: usize, high: usize) -> usize {
+    // pick a random element as pivot
+    let mut rng = rand::thread_rng();
+    arr.swap(low, rng.gen_range(low, high));
+    let e = arr[low].clone();
+
+    // arr[low+1:j] < v
+    let mut j = low;
+    // arr[j+1:i) > v
+    let mut i = low + 1;
+    while i <= high {
+        if arr[i].clone() < e {
+            j += 1;
+            arr.swap(j, i);
+        }
+        i += 1;
+    }
+
+    arr.swap(low, j);
+    j
+}
+
+pub fn sort_v2<T: Ord + Clone>(arr: &mut [T]) {
+    if arr.len() > 0 {
+        let high = arr.len() - 1;
+        quick_sort_v2(arr, 0, high)
     }
 }
 
@@ -52,6 +101,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -60,6 +110,7 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
 
     let n = 1000;
     let mut arr1 = util::generate_random_array(n, 1, n);
@@ -70,6 +121,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -78,6 +130,7 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
 
     let n = 10000;
     let mut arr1 = util::generate_random_array(n, 1, n);
@@ -88,6 +141,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -96,6 +150,21 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
+
+    let n = 100000;
+    let mut arr1 = util::generate_random_array(n, 1, n);
+    let mut arr2 = arr1.clone();
+    let mut arr3 = arr1.clone();
+    let mut arr4 = arr1.clone();
+    let mut arr5 = arr1.clone();
+    let mut arr6 = arr1.clone();
+    util::test_sort("merge sort_v1", super::merge::sort_v1, &mut arr1);
+    util::test_sort("heap sort_v1", super::heap::sort_v1, &mut arr2);
+    util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr3);
+    util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr4);
+    util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr5);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr6);
 
     let swap_times = 10;
     println!(
@@ -112,6 +181,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -120,6 +190,7 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
 
     let n = 1000;
     let mut arr1 = util::generate_nearly_ordered_array(n, swap_times);
@@ -130,6 +201,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -138,6 +210,7 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
 
     let n = 10000;
     let mut arr1 = util::generate_nearly_ordered_array(n, swap_times);
@@ -148,6 +221,7 @@ pub fn run() {
     let mut arr6 = arr1.clone();
     let mut arr7 = arr1.clone();
     let mut arr8 = arr1.clone();
+    let mut arr9 = arr1.clone();
     util::test_sort("selection sort", super::selection::sort, &mut arr1);
     util::test_sort("insertion sort_v1", super::insertion::sort_v1, &mut arr2);
     util::test_sort("insertion sort_v2", super::insertion::sort_v2, &mut arr3);
@@ -156,6 +230,26 @@ pub fn run() {
     util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr6);
     util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr7);
     util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr8);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr9);
+
+    let swap_times = 100;
+    println!(
+        "Test for nearly ordered array, swap_times = {} .",
+        swap_times
+    );
+    let n = 100000;
+    let mut arr1 = util::generate_nearly_ordered_array(n, swap_times);
+    let mut arr2 = arr1.clone();
+    let mut arr3 = arr1.clone();
+    let mut arr4 = arr1.clone();
+    let mut arr5 = arr1.clone();
+    let mut arr6 = arr1.clone();
+    util::test_sort("merge sort_v1", super::merge::sort_v1, &mut arr1);
+    util::test_sort("heap sort_v1", super::heap::sort_v1, &mut arr2);
+    util::test_sort("heap sort_v2", super::heap::sort_v2, &mut arr3);
+    util::test_sort("heap sort_v3", super::heap::sort_v3, &mut arr4);
+    util::test_sort("quick sort_v1", super::quick::sort_v1, &mut arr5);
+    util::test_sort("quick sort_v2", super::quick::sort_v2, &mut arr6);
 }
 
 #[cfg(test)]
@@ -167,12 +261,20 @@ mod tests {
         let mut res = vec![4, 1, 8, 5, 7];
         sort_v1(&mut res);
         assert_eq!(res, vec![1, 4, 5, 7, 8]);
+
+        let mut res = vec![4, 1, 8, 5, 7];
+        sort_v2(&mut res);
+        assert_eq!(res, vec![1, 4, 5, 7, 8]);
     }
 
     #[test]
     fn chars() {
         let mut res = vec!['A', 'a', 'h', 'b', 'W'];
         sort_v1(&mut res);
+        assert_eq!(res, vec!['A', 'W', 'a', 'b', 'h']);
+
+        let mut res = vec!['A', 'a', 'h', 'b', 'W'];
+        sort_v2(&mut res);
         assert_eq!(res, vec!['A', 'W', 'a', 'b', 'h']);
     }
 
@@ -181,10 +283,18 @@ mod tests {
         let mut res = Vec::<u8>::new();
         sort_v1(&mut res);
         assert_eq!(res, vec![]);
+
+        let mut res = Vec::<u8>::new();
+        sort_v2(&mut res);
+        assert_eq!(res, vec![]);
     }
 
     #[test]
     fn pre_sorted() {
+        let mut res = vec!['a', 'b', 'c', 'd'];
+        sort_v1(&mut res);
+        assert_eq!(res, vec!['a', 'b', 'c', 'd']);
+
         let mut res = vec!['a', 'b', 'c', 'd'];
         sort_v1(&mut res);
         assert_eq!(res, vec!['a', 'b', 'c', 'd']);
